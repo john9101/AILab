97c1a315407438eb2a02cf42e191441aae6ad5d6
john9101 <trinhdong1098@gmail.com>
2023-09-29 14:50:05 +0700

    first commit

---
+++ src/lab1/task2/AgentProgram.java
@@ -0,0 +1,17 @@
+package lab1.task2;
+
+import lab1.task2.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		if(p.getLocationState() == LocationState.DIRTY) {
+			return Environment.SUCK_DIRT;
+		}else if(p.getAgentLocation() == Environment.LOCATION_A){
+			return Environment.MOVE_RIGHT;
+		}else if(p.getAgentLocation() == Environment.LOCATION_B) {
+			return Environment.MOVE_LEFT;
+		}
+		return NoOpAction.NO_OP;
+	}
+}
